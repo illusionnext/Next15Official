da96617129813acdd8afac36fa98a2c21fa8a618
Illusion <17003685+faridnextjs@users.noreply.github.com>
2025-01-04 13:31:02 +0100

    added error boundaries LOL i have an error fix it by yourself

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,4 +1,5 @@
-"use cache";
+"use client";
+// "use cache";
 import { CustomerField } from "@/app/lib/definitions";
 import Link from "next/link";
 import {
@@ -8,15 +9,14 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
-import { createInvoice } from "@/app/lib/actions";
+import { createInvoice, State } from "@/app/lib/actions";
+import { useActionState } from "react";
 
-export default async function Form({
-  customers,
-}: {
-  customers: CustomerField[];
-}) {
+export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -29,6 +29,7 @@ export default async function Form({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -40,6 +41,14 @@ export default async function Form({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state?.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -57,6 +66,9 @@ export default async function Form({
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                autoComplete="on"
+                aria-describedby="customer-error"
+                // required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -77,6 +89,8 @@ export default async function Form({
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="customer-error"
+                  // required
                 />
                 <label
                   htmlFor="pending"
@@ -92,6 +106,8 @@ export default async function Form({
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="customer-error"
+                  // required
                 />
                 <label
                   htmlFor="paid"
